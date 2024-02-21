ce7f7ab0f9712e7c0d1dca5766484684b30a177f
Plombir4ik <plybomur720@gmail.com>
2024-02-21 23:11:11 +0200

    Database setup

---
+++ server/database/migrations/2024_02_21_210621_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name')->unique();
+            $table->string('description')->nullable();
+            $table->timestamps();
+
+            $table->index('user_id', 'category_user_idx');
+            $table->foreign('user_id', 'category_user_fk')->on('users')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
